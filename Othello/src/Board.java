1cc967ff35e040ac09cf86c678862539f6c13124
MinervaLu <mlu6@u.rochester.edu>
2015-04-17 18:54:36 -0400

    initilization

--- Othello/src/Board.java
+++ Othello/src/Board.java
@@ -1,7 +1,16 @@
 //testing whether can contribute or not
 public class Board {
-public static void main(String[] args){
-	System.out.println("Testing");
-	System.out.println("Another testing");
+int[][] state;
+public Board(){
+	//initialize the board
+	state=new int[8][8];
+	for(int i=0;i<state.length;i++){
+		for (int j=0;j<state[i].length;j++)
+			state[i][j]=-1;//empty
+	}
+	state[3][3]=1;//white
+	state[3][4]=0;//black
+	state[4][3]=1;//white
+	state[4][4]=0;
 }
 }
