e4ae97677154ae960fc1eb0f7196f4262f1b0973
MinervaLu <384815167@qq.com>
2015-04-18 14:23:54 -0400

    Update Board.java

--- Othello/src/Board.java
+++ Othello/src/Board.java
@@ -24,6 +24,7 @@ public Board(){
 	black.add(new Disk(4,3,-1));
 }
 
+//assume that the move is valid
 public void add(Disk d){
 	int row=d.row;
 	int col=d.col;
@@ -31,6 +32,8 @@ public void add(Disk d){
 	state[row][col]=color;
 	if (color==1){
 		white.add(new Disk(row,col,1));	
+		//flip the opponent
+		
 	}
 	else{
 		black.add(new Disk(row,col,-1));
@@ -45,16 +48,16 @@ public ArrayList<Disk> validMove(int color){
 	ArrayList<Disk> disk=new ArrayList<Disk>();
 	if (color==1){
 		for(int i=0;i<white.size();i++){
-			disk=combine(disk,checkHorizon(white.get(i)));
-			disk=combine(disk,checkVertical(white.get(i)));
-			disk=combine(disk,checkDiagonal(white.get(i)));
+			disk=combine(disk,findHorizon(white.get(i)));
+			disk=combine(disk,findVertical(white.get(i)));
+			disk=combine(disk,findDiagonal(white.get(i)));
 		}
 	}
 	else{
 		for(int i=0;i<black.size();i++){
-			disk=combine(disk,checkHorizon(black.get(i)));
-			disk=combine(disk,checkVertical(black.get(i)));
-			disk=combine(disk,checkDiagonal(black.get(i)));
+			disk=combine(disk,findHorizon(black.get(i)));
+			disk=combine(disk,findVertical(black.get(i)));
+			disk=combine(disk,findDiagonal(black.get(i)));
 		}
 	}
 	return disk;
@@ -63,7 +66,7 @@ public ArrayList<Disk> validMove(int color){
 
 
 //given a position, find possible position to create a outflank situation
-public ArrayList<Disk> checkHorizon(Disk d){
+public ArrayList<Disk> findHorizon(Disk d){
 	int row=d.row;
 	int col=d.col;
 	int color=d.color;
@@ -123,7 +126,7 @@ public ArrayList<Disk> checkHorizon(Disk d){
 
 
 
-public ArrayList<Disk> checkVertical(Disk d){
+public ArrayList<Disk> findVertical(Disk d){
 	int row=d.row;
 	int col=d.col;
 	int color=d.color;
@@ -179,7 +182,7 @@ public ArrayList<Disk> checkVertical(Disk d){
 }
 
 
-public ArrayList<Disk> checkDiagonal(Disk d){
+public ArrayList<Disk> findDiagonal(Disk d){
 	int row=d.row;
 	int col=d.col;
 	int color=d.color;
@@ -244,6 +247,145 @@ public ArrayList<Disk> checkDiagonal(Disk d){
 
 
 
+public void update(Disk d){
+	int row=d.row;
+	int col=d.col;
+	int color=d.color;
+	Disk left=new Disk(-1,-1,0);
+	Disk right=new Disk(-1,-1,0);
+	Disk up=new Disk(-1,-1,0);
+	Disk down=new Disk(-1,-1,0);
+	int maxleft=-1;
+	int minright=8;
+	int mindown=8;
+	int maxup=-1;
+	if(color==1){
+		for(int i=0;i<white.size();i++){
+			if(white.get(i).row==row&&white.get(i).col<col){
+				if(white.get(i).col>maxleft){
+			    maxleft=white.get(i).col;
+				left=white.get(i);
+				}
+			}
+			else if(white.get(i).row==row&&white.get(i).col>col){
+				if(white.get(i).col<minright){
+					minright=white.get(i).col;
+					right=white.get(i);
+				}
+			}
+			else if(white.get(i).col==col&&white.get(i).row<row){
+				if(white.get(i).row>maxup){
+					maxup=white.get(i).row;
+					up=white.get(i);
+				}
+			}
+				
+			else if(white.get(i).col==col&&white.get(i).row>row){
+				if(white.get(i).row<mindown){
+					mindown=white.get(i).row;
+					down=white.get(i);
+				}
+			}
+		}
+	}
+	
+	else{
+		for(int i=0;i<black.size();i++){
+			if(black.get(i).row==row&&black.get(i).col<col){
+				if(black.get(i).col>maxleft){
+					maxleft=black.get(i).col;
+					left=black.get(i);
+				}
+			}
+			else if(black.get(i).row==row&&black.get(i).col>col){
+				if(black.get(i).col<minright){
+					minright=black.get(i).col;
+					right=black.get(i);
+				}
+			}
+			else if(black.get(i).col==col&&black.get(i).row<row){
+				if(black.get(i).row>maxup){
+					maxup=black.get(i).row;
+					up=black.get(i);
+				}
+			}
+				
+			else if(black.get(i).col==col&&black.get(i).row>row){
+				if(black.get(i).row<mindown){
+					mindown=black.get(i).row;
+					down=black.get(i);
+				}
+			}
+		}
+	}
+	
+	/*
+	 int maxleft=-1;
+	int minright=8;
+	int mindown=8;
+	int maxup=-1;
+	 */
+	if(maxleft!=-1&&checkAllSameHorizon(maxleft,col,row)){
+	    updateHorizon(maxleft,col,row);	
+	}
+	
+	if(minright!=8&&checkAllSameHorizon(col,minright,row)){
+		updateHorizon(col,minright,row);
+	}
+	
+	if(maxup!=-1&&checkAllSameVertical(maxup,row,col)){
+	    updateVertical(maxup,row,col);	
+	}
+	
+	if(mindown!=8&&checkAllSameVertical(row,mindown,col)){
+		updateVertical(row,mindown,col);
+	}
+	
+}
+
+
+
+
+public boolean checkAllSameHorizon(int i,int j,int row){
+	int n=state[row][i+1];
+	for (int k=i+2;k<j;k++){
+		if(state[row][k]!=n)
+			return false;
+	}
+	return true;
+}
+
+
+public boolean checkAllSameVertical(int i,int j,int col){
+	int n=state[i+1][col];
+	for(int k=i+2;k<j;k++){
+		if(state[k][col]!=n)
+			return false;
+	}
+	return true;
+}
+
+public void updateHorizon(int i,int j,int row){
+	for(int k=i+1;k<j;k++)
+		state[row][k]=-state[row][k];
+}
+
+public void updateVertical(int i,int j,int col){
+	for(int k=i+1;k<j;k++)
+		state[k][col]=-state[k][col];
+}
+
+public void updateDiagonal(int i,int j,int row, int col){
+	for (int k=1;k<row-i;k++){
+		state[i+k][j+k]=-state[i+k][j+k];
+	}
+}
+//////////
+//unfinished
+//////////
+public boolean checkAllSameDiagonal(int i,int j,int row,int col){
+	return false;
+}
 public static ArrayList<Disk> combine(ArrayList<Disk> d1, ArrayList<Disk> d2){
 	if(d2.isEmpty()){
 		return d1;
