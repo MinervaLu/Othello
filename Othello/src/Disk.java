3315c78b89c4e5da8dc60eca0bda5426afd1ac4a
MinervaLu <384815167@qq.com>
2015-04-18 19:52:47 -0400

    Update Disk.java

--- Othello/src/Disk.java
+++ Othello/src/Disk.java
@@ -1,11 +1,16 @@
 //class for disk
 public class Disk {
 int color;
-int[][] position;
+int row;
+int col;
 
-public Disk(int[][] position,int color){
-	this.position=position;
+public Disk(int row,int col,int color){
+	this.row=row;
+	this.col=col;
 	this.color=color;
 }
 
+public String toString(){
+	return String.format("(%d, %d) color %s.\n",row,col,color==1?"white":"black");
+}
 }
